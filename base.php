65cc501517ccd9675d88c2784769f109c7a93651
zekaeko.p <zekaeko.p@gmail.com>
2026-04-12 00:15:41 +0300

    added base.php that includes non existing files such as header and footer

---
+++ base.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+include("header.php");
+include("footer.php");
+
+?>
+    
+</body>
+</html>
