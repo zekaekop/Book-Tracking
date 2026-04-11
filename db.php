9f7b10503a080b1c928ec7ab492e8bbe3e53710c
zekaeko.p <zekaeko.p@gmail.com>
2026-04-12 00:03:27 +0300

    fixed syntax issues in db.php

--- db.php
+++ db.php
@@ -6,8 +6,8 @@ $usr = "root";
 $pwd = "eko";
 
 try{
-    $pdo = new PDO("mysql:host=$host;db_name=$db", $usr, $pwd);
-    $pdo -> setAttribute(PDO::ATTR_ERROR, PDO:ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $usr, $pwd);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (EXCEPTION $e) {
     die("Connection with the DB failed" . $e->getMessage());
 }
