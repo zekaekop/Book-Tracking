8bfb73e428fb7c0e3a7718e6df6081b67899f0ed
zekaeko.p <zekaeko.p@gmail.com>
2026-04-13 00:38:56 +0300

    Added header with add_books.php and statistics.php

---
+++ header.php
@@ -0,0 +1,8 @@
+
+<nav>
+    <ul style="list-style-type: none;">
+        <li><a href="home.php">Home</a></li>
+        <li><a href="add_books.php">Add Books</a></li>
+        <li><a href="statistics.php">Statistics</a></li>
+    </ul>
+</nav>
