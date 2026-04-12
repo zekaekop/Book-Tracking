34a95db426cfa42e11a2b152dde9319c95e06709
zekaeko.p <zekaeko.p@gmail.com>
2026-04-13 01:02:41 +0300

    users can now add books and the books also track who it was added by

---
+++ add_books.php
@@ -0,0 +1,27 @@
+<?php
+
+include("base.php");
+
+redirect_unauth_users();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["add_book_submit"])) {
+
+        $title = $_POST["title"];
+        $desc = $_POST["desc"];
+
+        $query = $pdo->prepare("INSERT INTO books (`title`, `desc`, `added_by_user_id`) VALUES (?,?,?)");
+        $query->execute([$title, $desc, $_SESSION["user"]["id"]]);
+    }
+}
+?>
+
+<form method="POST" action="">
+
+    <input type="text" name="title" placeholder="Title" required>
+    <br>
+    <textarea name="desc" placeholder="Description" required></textarea>
+    <br>
+    <button type="submit" name="add_book_submit">Add Book</button>
+
+</form>
