59293d31c1ea0673a3caf347f614d6d8cbe2c98f
zekaeko.p <zekaeko.p@gmail.com>
2026-04-16 23:35:36 +0300

    added footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <footer class="footer_card">
+        <p class="text-center">
+            <small>MIT - License</small>
+            <small>Copyright (c) 2026 Eko</small>
+        </p>
+    </footer>
+</div>
