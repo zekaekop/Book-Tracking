5a01ce50e18cc8b4ac953778a738d0c7d349b4c8
zekaeko.p <zekaeko.p@gmail.com>
2026-04-19 13:35:58 +0300

    removed home from header and made the project title the home button

--- src/template/header.php
+++ src/template/header.php
@@ -1,9 +1,8 @@
 <div class="container w-100">
     <div class="nav navbar header_card">
         <nav class="d-flex w-100">
-            <h5 class="mt-2">Book Tracking</h5>
+            <h5 class="mt-2"><a class="nav-link text-black" href="home.php">Book Tracking</a></h5>
             <ul class="nav ms-auto" style="list-style-type: none;">
-                <li class="nav-item"><a class="nav-link text-black" href="home.php">Home</a></li>
                 <li class="nav-item"><a class="nav-link text-black" href="books.php">Books</a></li>
                 <li class="nav-item"><a class="nav-link text-black" href="add_books.php">Add Books</a></li>
                 <li class="nav-item"><a class="nav-link text-black" href="statistics.php">Statistics</a></li>
