6075bb8edfdc924bd3f2286e7988c32bf64c18b5
zekaeko.p <zekaeko.p@gmail.com>
2026-04-19 12:58:17 +0300

    adds div container to register.php

--- public/register.php
+++ public/register.php
@@ -7,20 +7,22 @@ require_once __DIR__  . "/../src/auth.php";
 $register_status = "";
 ?>
 
-<form method="POST">
+<div class="container">
+    <form method="POST">
 
-    <p><?= htmlspecialchars($register_status) ?></p>
+        <p><?= htmlspecialchars($register_status) ?></p>
 
-    <input type="text" name="username" placeholder="Username" required>
-    <br>
-    <input type="text" name="email" placeholder="Email" required>
-    <br>
-    <input type="password" name="password" placeholder="Password" required>
-    <br>
-    <input type="password" name="repeat_password" placeholder="Repeat Password" required>
-    <br>
-    <button type="submit" name="register_submit">Register</button>
+        <input type="text" name="username" placeholder="Username" required>
+        <br>
+        <input type="text" name="email" placeholder="Email" required>
+        <br>
+        <input type="password" name="password" placeholder="Password" required>
+        <br>
+        <input type="password" name="repeat_password" placeholder="Repeat Password" required>
+        <br>
+        <button type="submit" name="register_submit">Register</button>
 
-    <a href="login.php"><small>have an account?</small></a>
+        <a href="login.php"><small>have an account?</small></a>
 
-</form>
+    </form>
+</div>
