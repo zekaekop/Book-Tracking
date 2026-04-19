747f5cf59604cb9bbf3cbac9f7bfd40af093f991
zekaeko.p <zekaeko.p@gmail.com>
2026-04-19 12:49:45 +0300

    made the header look way nicer

--- src/template/header.php
+++ src/template/header.php
@@ -1,12 +1,12 @@
 <div class="container w-100">
     <div class="nav navbar header_card">
-        <nav class="d-flex">
-            <h5>Book Tracking</h5>
-            <ul class="nav w-100" style="list-style-type: none;">
-                <li class="nav-item"><a href="home.php">Home</a></li>
-                <li class="nav-item"><a href="add_books.php">Add Books</a></li>
-                <li class="nav-item"><a href="statistics.php">Statistics</a></li>
-                <li class="nav-item"><a href="account.php">Account</a></li>
+        <nav class="d-flex w-100">
+            <h5 class="mt-2">Book Tracking</h5>
+            <ul class="nav ms-auto" style="list-style-type: none;">
+                <li class="nav-item"><a class="nav-link text-black" href="home.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link text-black" href="add_books.php">Add Books</a></li>
+                <li class="nav-item"><a class="nav-link text-black" href="statistics.php">Statistics</a></li>
+                <li class="nav-item"><a class="nav-link text-black" href="account.php">Account</a></li>
             </ul>
         </nav>
     </div>
