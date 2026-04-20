65c0db7063e36fe10345bef074bfd971e336198b
zekaeko.p <zekaeko.p@gmail.com>
2026-04-21 01:56:31 +0300

    added offset to header links

--- src/template/header.php
+++ src/template/header.php
@@ -3,10 +3,10 @@
         <nav class="d-flex w-100 book-card ">
             <h5 class="mt-2"><a class="nav-link text-white" href="home.php">Book Tracking</a></h5>
             <ul class="nav ms-auto" style="list-style-type: none;">
-                <li class="nav-item"><a class="nav-link text-white" href="books.php">Books</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="add_books.php">Add Books</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="statistics.php">Statistics</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="account.php">Account</a></li>
+                <li class="nav-item"><a class="mt-2 nav-link text-white" href="books.php">Books</a></li>
+                <li class="nav-item"><a class="mt-2 nav-link text-white" href="add_books.php">Add Books</a></li>
+                <li class="nav-item"><a class="mt-2 nav-link text-white" href="statistics.php">Statistics</a></li>
+                <li class="nav-item"><a class="mt-2 nav-link text-white" href="account.php">Account</a></li>
             </ul>
         </nav>
     </div>
