f4d734079c2b21ca9bb822422124523a6dbd4cdf
zekaeko.p <zekaeko.p@gmail.com>
2026-04-21 03:20:28 +0300

    polished detail page style

--- public/detail.php
+++ public/detail.php
@@ -83,95 +83,100 @@ And books.php will allow a more advanced way of using the site with searches fil
 -->
 
 <div class="container">
-    <h1>Browse Books</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda distinctio cupiditate quibusdam iste magnam officia in maiores laboriosam obcaecati, quas repellat itaque, optio quidem. Incidunt dolorem cum reiciendis placeat fuga?</p>
+    <div class="book-card mb-4">
 
-    <hr>
+        <div class="background-shadow-shade">
+            <h1>Browse Books</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda distinctio cupiditate quibusdam iste magnam officia in maiores laboriosam obcaecati, quas repellat itaque, optio quidem. Incidunt dolorem cum reiciendis placeat fuga?</p>
+        </div>
 
-    <h2>Book Information</h2>
+        <hr>
 
-    <hr>
+        <h2>Book Information</h2>
 
-    <form method="POST" action="">
-        <li class="compact_book_gray mb-3">
-            <h3>Title: <?= htmlspecialchars($book["title"]);?></h3>
-            <h4>Description: <?= htmlspecialchars($book["desc"]);?></h4>
-            <h4>Book ID: <?= htmlspecialchars($book["book_id"]);?></h4>
+        <hr>
 
-            <!-- If the book owner is the user -->
-            <?php if ($_SESSION["user"]["username"] == $book["username"]): ?>
+        <form method="POST" action="">
+            <li class="background-shadow-shade w-100 mb-3">
+                <h3>Title: <?= htmlspecialchars($book["title"]);?></h3>
+                <h4>Description: <?= htmlspecialchars($book["desc"]);?></h4>
+                <h4>Book ID: <?= htmlspecialchars($book["book_id"]);?></h4>
 
-                <!-- He has access to changing availablity and category -->
-                <h4>Status: <input type="checkbox" value="status_check" name="status_checkbox"
-                <?php if ($book["status"] == "available"): ?>
-                    checked
-                <?php endif ?>
-                ><?= htmlspecialchars($book["status"]);?></h4>
+                <!-- If the book owner is the user -->
+                <?php if ($_SESSION["user"]["username"] == $book["username"]): ?>
+
+                    <!-- He has access to changing availablity and category -->
+                    <h4>Status: <input type="checkbox" value="status_check" name="status_checkbox"
+                    <?php if ($book["status"] == "available"): ?>
+                        checked
+                    <?php endif ?>
+                    ><?= htmlspecialchars($book["status"]);?></h4>
 
 
-                <label for="categories_select">Choose a Category:</label>
-                <select name="category_select">
-                    <?php 
-                    $query = $pdo->query("SELECT * FROM categories");
-                    $categories = $query->fetchAll();
+                    <label for="categories_select">Choose a Category:</label>
+                    <select name="category_select">
+                        <?php 
+                        $query = $pdo->query("SELECT * FROM categories");
+                        $categories = $query->fetchAll();
 
-                    foreach ($categories as $category): ?>
+                        foreach ($categories as $category): ?>
 
-                        <!-- Selected becomes default for the value it has in books -->
-                        <option 
-                        <?php if ($category["name"] == $book["category_name"]): ?>
-                            selected
-                        <?php endif ?>
-                        
-                        value="<?= $category["id"]; ?>"
-                        ><?= htmlspecialchars($category["name"]); ?></option>
-                    <?php endforeach ?>
-                </select> 
+                            <!-- Selected becomes default for the value it has in books -->
+                            <option 
+                            <?php if ($category["name"] == $book["category_name"]): ?>
+                                selected
+                            <?php endif ?>
+                            
+                            value="<?= $category["id"]; ?>"
+                            ><?= htmlspecialchars($category["name"]); ?></option>
+                        <?php endforeach ?>
+                    </select> 
 
-            <?php else: ?>
-                <h4>Status: <?= htmlspecialchars($book["status"]);?></h4>
-            <?php endif ?>
-
-            <h4>Requested By: 
-            <?php     
-            if ($book["requested_user_id"]) {
-                $can_request = false;
-                echo htmlspecialchars($book["requested_username"]);
-            } else {
-                $can_request = true;
-                echo "No one";
-            } 
-            ?>
-            </h4>
-            
-            <h4>Added By: <?= htmlspecialchars($book["username"]);?></h4>
-
-            <h4>Category: <?= htmlspecialchars($book["category_name"]);?></h4>
-
-            <?php if ($book["status"] == "available"): ?>
-                <?php if ($can_request && $_SESSION["user"]["username"] != $book["username"]): ?>   
-                <button type="submit" 
-                                name="book_request_submit" 
-                                value="<?= $book["book_id"] ?>"
-                                class="ms-auto">Request</button>
-                <?php elseif ($_SESSION["user"]["username"] != $book["username"]): ?>
-                <button type="submit" 
-                                name="book_unrequest_submit" 
-                                value="<?= $book["book_id"] ?>"
-                                class="ms-auto">Remove Request</button>
                 <?php else: ?>
-                <button class="ms-auto" value="<?= $book["book_id"] ?>" name="book_remove_submit">Remove Book</button>
-                <button class="ms-auto" value="<?= $book["book_id"] ?>" name="book_status_update_submit">Apply Status</button>
+                    <h4>Status: <?= htmlspecialchars($book["status"]);?></h4>
                 <?php endif ?>
-            <?php else: ?>
-                                <button type="submit" 
-                                name="book_unrequest_submit" 
-                                value="<?= $book["book_id"] ?>"
-                                class="ms-auto">The book is unavailable</button>
-            <?php endif ?>
-        </li>
-    </form>
 
+                <h4>Requested By: 
+                <?php     
+                if ($book["requested_user_id"]) {
+                    $can_request = false;
+                    echo htmlspecialchars($book["requested_username"]);
+                } else {
+                    $can_request = true;
+                    echo "No one";
+                } 
+                ?>
+                </h4>
+                
+                <h4>Added By: <?= htmlspecialchars($book["username"]);?></h4>
+
+                <h4>Category: <?= htmlspecialchars($book["category_name"]);?></h4>
+
+                <?php if ($book["status"] == "available"): ?>
+                    <?php if ($can_request && $_SESSION["user"]["username"] != $book["username"]): ?>   
+                    <button type="submit" 
+                                    name="book_request_submit" 
+                                    value="<?= $book["book_id"] ?>"
+                                    class="ms-auto">Request</button>
+                    <?php elseif ($_SESSION["user"]["username"] != $book["username"]): ?>
+                    <button type="submit" 
+                                    name="book_unrequest_submit" 
+                                    value="<?= $book["book_id"] ?>"
+                                    class="ms-auto">Remove Request</button>
+                    <?php else: ?>
+                    <button class="ms-auto" value="<?= $book["book_id"] ?>" name="book_remove_submit">Remove Book</button>
+                    <button class="ms-auto" value="<?= $book["book_id"] ?>" name="book_status_update_submit">Apply Status</button>
+                    <?php endif ?>
+                <?php else: ?>
+                                    <button type="submit" 
+                                    name="book_unrequest_submit" 
+                                    value="<?= $book["book_id"] ?>"
+                                    class="ms-auto">The book is unavailable</button>
+                <?php endif ?>
+            </li>
+        </form>
+
+    </div>
 </div>
 
 <?php require_once __DIR__ . '/../src/template/base_bottom.php'; ?>
